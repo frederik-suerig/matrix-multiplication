23f2708bf2e11e4537c6bdd32b6930f3bc6025f5
Frederik Suerig <54886730+frederik-suerig@users.noreply.github.com>
2022-02-25 13:29:40 +0100

    feat: return error when matrix size is invalid

--- matrix.go
+++ matrix.go
@@ -1,6 +1,15 @@
 package main
 
-func MatrixMultiplication(matrix1, matrix2 [][]int) [][]int {
+import "errors"
+
+func MatrixMultiplication(matrix1, matrix2 [][]int) ([][]int, error) {
+	rows := len(matrix1)
+	cols := len(matrix2[0])
+
+	if rows != cols {
+		return nil, errors.New("invalid input, those matrixes can't be multiplied")
+	}
+
 	result := [][]int{}
 
 	for i := 0; i < len(matrix1); i++ {
@@ -17,5 +26,5 @@ func MatrixMultiplication(matrix1, matrix2 [][]int) [][]int {
 
 	}
 
-	return result
+	return result, nil
 }
