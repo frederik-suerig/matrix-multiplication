d238d713716308c9d8119c7872f87a60b0a53aa7
Frederik Suerig <54886730+frederik-suerig@users.noreply.github.com>
2022-03-02 10:28:35 +0100

    test: Add tests & benchmarks
    - Sqaure Matrix
    - Multi threaded matrix multiplication

--- matrix_test.go
+++ matrix_test.go
@@ -39,6 +39,22 @@ func TestMatrixMultiplication(t *testing.T) {
 		}
 	})
 
+	t.Run("multiply 4x4 by 4x4 matrix", func(t *testing.T) {
+		m1 := [][]int{{5, 2, 6, 1}, {0, 6, 2, 0}, {3, 8, 1, 4}, {1, 8, 5, 6}}
+		m2 := [][]int{{7, 5, 8, 0}, {1, 8, 2, 6}, {9, 4, 3, 8}, {5, 3, 7, 9}}
+
+		got, err := MatrixMultiplication(m1, m2)
+		want := [][]int{{96, 68, 69, 69}, {24, 56, 18, 52}, {58, 95, 71, 92}, {90, 107, 81, 142}}
+
+		if err != nil {
+			t.Errorf("Received an error %q when it shouldn't", err)
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v, got %v", want, got)
+		}
+	})
+
 	t.Run("expect error when matrices rows and columns don't match", func(t *testing.T) {
 		m1 := [][]int{{3, 2, 1}, {1, 0, 2}}
 		m2 := [][]int{{3}, {1}}
@@ -51,10 +67,57 @@ func TestMatrixMultiplication(t *testing.T) {
 	})
 }
 
+func TestMultiThreadMatrixMultiplication(t *testing.T) {
+	t.Run("multiply a small sqaure matrix", func(t *testing.T) {
+		m1 := [][]int{{5, 2, 6, 1}, {0, 6, 2, 0}, {3, 8, 1, 4}, {1, 8, 5, 6}}
+		m2 := [][]int{{7, 5, 8, 0}, {1, 8, 2, 6}, {9, 4, 3, 8}, {5, 3, 7, 9}}
+
+		got, err := MultiThreadedMatrixMultiplication(m1, m2)
+		want := [][]int{{96, 68, 69, 69}, {24, 56, 18, 52}, {58, 95, 71, 92}, {90, 107, 81, 142}}
+
+		if err != nil {
+			t.Errorf("Received an error %q when it shouldn't", err)
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v, got %v", want, got)
+		}
+	})
+
+	t.Run("expect error when matrices rows and columns don't match", func(t *testing.T) {
+		m1 := [][]int{{3, 2, 1}, {1, 0, 2}}
+		m2 := [][]int{{3}, {1}}
+
+		_, err := MultiThreadedMatrixMultiplication(m1, m2)
+
+		if err == nil {
+			t.Errorf("Expected error, but got %v", err)
+		}
+	})
+}
+
 func BenchmarkMatrixMultiplication(b *testing.B) {
 	m1 := [][]int{{3, 2, 1}, {1, 0, 2}}
 	m2 := [][]int{{1, 2}, {0, 1}, {4, 0}}
 	for i := 0; i < b.N; i++ {
 		MatrixMultiplication(m1, m2)
+	}
+}
+
+func BenchmarkSqaureMatrixMultiplication(b *testing.B) {
+	m1 := [][]int{{5, 2, 6, 1}, {0, 6, 2, 0}, {3, 8, 1, 4}, {1, 8, 5, 6}}
+	m2 := [][]int{{7, 5, 8, 0}, {1, 8, 2, 6}, {9, 4, 3, 8}, {5, 3, 7, 9}}
+
+	for i := 0; i < b.N; i++ {
+		MatrixMultiplication(m1, m2)
+	}
+}
+
+func BenchmarkMultiThreadedMatrixMultiplication(b *testing.B) {
+	m1 := [][]int{{5, 2, 6, 1}, {0, 6, 2, 0}, {3, 8, 1, 4}, {1, 8, 5, 6}}
+	m2 := [][]int{{7, 5, 8, 0}, {1, 8, 2, 6}, {9, 4, 3, 8}, {5, 3, 7, 9}}
+
+	for i := 0; i < b.N; i++ {
+		MultiThreadedMatrixMultiplication(m1, m2)
 	}
 }
