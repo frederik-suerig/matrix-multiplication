09c49e4d75415f696c0773b263c87274c07ebde0
Frederik Suerig <54886730+frederik-suerig@users.noreply.github.com>
2022-03-02 15:44:55 +0100

    feat: multi-thread matrix multiplication

--- matrix.go
+++ matrix.go
@@ -1,19 +1,23 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 func MatrixMultiplication(matrix1, matrix2 [][]int) ([][]int, error) {
-	if checkMatrixSize(matrix1, matrix2) {
+	rows := len(matrix1)
+	columns := len(matrix2[0])
+
+	if rows != columns {
 		return nil, errors.New("invalid input, those matrixes can't be multiplied")
 	}
 
-	columns := len(matrix2[0])
-
 	result := [][]int{}
 
-	for i := 0; i < len(matrix1); i++ {
+	for i := 0; i < rows; i++ {
 		result = append(result, make([]int, columns))
-		for j := 0; j < len(matrix2[0]); j++ {
+		for j := 0; j < columns; j++ {
 			for k := 0; k < len(matrix2); k++ {
 				result[i][j] += matrix1[i][k] * matrix2[k][j]
 			}
@@ -24,9 +28,32 @@ func MatrixMultiplication(matrix1, matrix2 [][]int) ([][]int, error) {
 	return result, nil
 }
 
-func checkMatrixSize(m1, m2 [][]int) bool {
-	rows := len(m1)
-	cols := len(m2[0])
+func MultiThreadedMatrixMultiplication(matrix1, matrix2 [][]int) ([][]int, error) {
+	rows := len(matrix1)
+	columns := len(matrix2[0])
 
-	return rows != cols
+	if rows != columns {
+		return nil, errors.New("invalid input, those matrixes can't be multiplied")
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(rows)
+
+	result := [][]int{}
+
+	for i := 0; i < rows; i++ {
+		result = append(result, make([]int, columns))
+		go func(i int) {
+			defer wg.Done()
+			for j := 0; j < columns; j++ {
+				for k := 0; k < len(matrix2); k++ {
+					result[i][j] += matrix1[i][k] * matrix2[k][j]
+				}
+			}
+		}(i)
+	}
+
+	wg.Wait()
+
+	return result, nil
 }
